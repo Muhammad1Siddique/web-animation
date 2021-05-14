b8af487e43396f357caafd47d3483fefd3602e01
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-05-14 18:46:41 +0500

    animation five rotate updated

--- animation5/animation.js
+++ animation5/animation.js
@@ -1,10 +1,10 @@
 var boxs = document.querySelector('#box');
 boxs.animate([
-    {transform: 'translate(0, 0) scale(.5)', background: 'yellow'},
-    {transform: 'translate(400px, 0) scale(1)', background: 'green'},
-    {transform: 'translate(400px, 400px) scale(1.5)', background: 'blue'},
-    {transform: 'translate(0, 400px) scale(1)', background: 'red'},
-    {transform: 'translate(0, 0) scale(.5)', background: 'orange'},
+    {transform: 'translate(0, 0) scale(.5) rotate(0deg)', background: 'yellow'},
+    {transform: 'translate(400px, 0) scale(1) rotate(90deg)', background: 'green'},
+    {transform: 'translate(400px, 400px) scale(1.5) rotate(180deg)', background: 'blue'},
+    {transform: 'translate(0, 400px) scale(1) rotate(270deg)', background: 'red'},
+    {transform: 'translate(0, 0) scale(.5) rotate(360deg)', background: 'orange'},
 ],{
     duration: 5000,
     iterations: Infinity
