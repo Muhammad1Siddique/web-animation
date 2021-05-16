533d36569ab02996ee51c00dca1ea591866cc7e0
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-05-16 09:08:56 +0500

    Project A4 Added

---
+++ Project4A/main.js
@@ -0,0 +1,127 @@
+var cloud1 = document.querySelector('#cloud1');
+
+cloud1.animate([
+    {transform: 'translate(0, 0)'},
+    {transform: 'translate(1100px, 0)'}
+    
+],{
+    duration: 4500,
+    iterations: Infinity,
+    direction: 'alternate'
+})
+
+var cloud2 = document.querySelector('#cloud2');
+
+cloud2.animate([
+    {transform: 'translate(0, 20px)'},
+    {transform: 'translate(1100px, 20px)'}
+    
+],{
+    duration: 5000,
+    iterations: Infinity,
+    direction: 'alternate-reverse'
+})
+
+var cloud3 = document.querySelector('#cloud3');
+
+cloud3.animate([
+    {transform: 'translate(0, 50px)'},
+    {transform: 'translate(1100px, 50px)'}
+    
+],{
+    duration: 4200,
+    iterations: Infinity,
+    direction: 'alternate'
+})
+
+
+var cloud4 = document.querySelector('#cloud4');
+
+cloud4.animate([
+    {transform: 'translate(0, 10px)'},
+    {transform: 'translate(1100px, 0)'}
+    
+],{
+    duration: 4000,
+    iterations: Infinity,
+    direction: 'alternate'
+})
+
+var cloud5 = document.querySelector('#cloud5');
+
+cloud5.animate([
+    {transform: 'translate(0, 30px)'},
+    {transform: 'translate(1100px, 30px)'}
+    
+],{
+    duration: 5500,
+    iterations: Infinity,
+    direction: 'alternate-reverse'
+})
+
+var cloud6 = document.querySelector('#cloud6');
+
+cloud6.animate([
+    {transform: 'translate(0, 50px)'},
+    {transform: 'translate(1100px, 50px)'}
+    
+],{
+    duration: 5200,
+    iterations: Infinity,
+    direction: 'alternate-reverse'
+})
+
+var cloud7 = document.querySelector('#cloud7');
+
+cloud7.animate([
+    {transform: 'translate(0, 80px)'},
+    {transform: 'translate(1100px, 80px)'}
+    
+],{
+    duration: 5800,
+    iterations: Infinity
+})
+
+var cloud8 = document.querySelector('#cloud8');
+
+cloud8.animate([
+    {transform: 'translate(0, 100px)'},
+    {transform: 'translate(1100px, 100px)'}
+    
+],{
+    duration: 4300,
+    iterations: Infinity
+})
+
+var cloud9 = document.querySelector('#cloud9');
+
+cloud9.animate([
+    {transform: 'translate(0, 400px)'},
+    {transform: 'translate(1100px, 120px)'}
+    
+],{
+    duration: 5100,
+    iterations: Infinity
+})
+
+var cloud10 = document.querySelector('#cloud10');
+
+cloud10.animate([
+    {transform: 'translate(0, 140px)'},
+    {transform: 'translate(1100px, 140px)'}
+    
+],{
+    duration: 4700,
+    iterations: Infinity
+})
+
+var cloud11 = document.querySelector('#cloud11');
+
+cloud11.animate([
+    {transform: 'translate(0, 130px)'},
+    {transform: 'translate(1100px, 130px)'}
+    
+],{
+    duration: 4900,
+    iterations: Infinity
+})
